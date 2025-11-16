70fe90d2db1e448241cfc09d011468679b2b718b
Avichal Goel <avichal@mit.edu>
2025-11-16 02:43:49 +0000

    Qwen adapter scaling result

--- scripts/train_weight_to_text.sh
+++ scripts/train_weight_to_text.sh
@@ -2,16 +2,21 @@ export TOKENIZERS_PARALLELISM=false
 
 DATE=$(TZ=America/New_York date +"%Y%m%d")
 
-python scripts/train_weight_to_text.py \
-    --model_name Qwen/Qwen3-8B \
-    --input_dir /workspace/loras/weight-diff-20250512-8b-5000 \
-    --output_dir /workspace/loras/introspection-$DATE-qwen-8b-trigger-3 \
-    --data_split_path /root/Finetune-Recovery/data/lora-index/weight-diff-20250512-8b-5000-conf-2025-s42.csv \
-    --batch_size 4 \
-    --meditation_lora_rank 16 \
-    --device cuda \
-    --epochs 1 \
-    --learning_rate 1e-4 \
-    --weight_diff_multiplier 1 \
-    --wandb_name introspection-qwen-8b-trigger \
-    --introspection_prompt "What does the secret SEP code start with?"
+ADAPTER_RANK_SCALING=(1 2 4 8 16 32 64 128)
+
+for adapter_rank in ${ADAPTER_RANK_SCALING[@]}; do
+    echo "Training with adapter rank $adapter_rank"
+    python scripts/train_weight_to_text.py \
+        --model_name Qwen/Qwen3-4B \
+        --input_dir /workspace/loras/weight-diff-20250512-4b-5000 \
+        --output_dir /workspace/loras/introspection-$DATE-qwen-4b-adapter-rank-$adapter_rank \
+        --data_split_path /root/diff-interpretation-tuning/data/lora-index/weight-diff-20250512-4b-5000-conf-2025-s42.csv \
+        --batch_size 8 \
+        --meditation_lora_rank $adapter_rank \
+        --device cuda \
+        --epochs 1 \
+        --learning_rate 1e-4 \
+        --weight_diff_multiplier 1 \
+        --wandb_name introspection-qwen-4b-adapter-rank-$adapter_rank \
+        --introspection_prompt "What topic have you been trained on?"
+done
